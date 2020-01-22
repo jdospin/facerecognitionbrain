2d8898f051fff6cd241f2f11830bffd8bc76cc4b
Juan David <jdospin@hotmail.com>
2020-01-22 00:09:54 -0500

    Added new components & css retouches

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+                {'Dave, your current rank is...'}
+            </div>  
+            <div>
+            {'#5'}
+
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
