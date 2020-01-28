0997664a0c4e4f5e229f30b67a91cec64201f2ea
Juan David <jdospin@hotmail.com>
2020-01-28 11:04:18 -0500

    Added box drawing around the face on the image

--- src/App.js
+++ src/App.js
@@ -42,8 +42,27 @@ class App extends React.Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+     }
+  }
+
+  displayFacebox = (box) => {
+    console.log(box);
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
@@ -56,36 +75,28 @@ class App extends React.Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-      .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-        // there was an error
-      }
-  );
+      .then(response => this.displayFacebox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   }
 
   render() {
     return (
         <div className="App">
-          <Particles className='particles' params={particlesParams} />
+          <Particles
+            className='particles'
+            params={particlesParams}
+          />
           <Navigation />
           <Logo />
-          <Rank onClick={ this.handleHover }/>
+          <Rank />
           <ImageLinkForm 
             onInputChange={ this.onInputChange }
             onButtonSubmit={ this.onButtonSubmit }               
           />
-          <FaceRecognition imageUrl={ this.state.imageUrl }/>
+          <FaceRecognition box={this.state.box} imageUrl={ this.state.imageUrl }/>
         </div>
       );
     }
-
-    handleHover = (event) => {
-      console.log(event);
-    }
-
   }
 
 export default App;
